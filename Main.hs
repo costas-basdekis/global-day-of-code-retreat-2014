a8195977f4ce3b6b5a9815c1b2727fced330c56b
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-15 11:46:14 +0000

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+main = do
+	putStrLn "hello, world"
+
+data Conway = Empty
+
+nextConway :: Conway -> Conway
+nextConway = undefined
---
+++ test.hs
@@ -0,0 +1,7 @@
+module Conway_Test where
+
+import Main hiding (main)
+import Test.HUnit
+
+main = runTestTT $ TestList [
+	]
