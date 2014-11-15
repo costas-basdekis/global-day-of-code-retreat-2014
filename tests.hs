e10d8b40ea61ba1c29b985284ab2266faa3b8b59
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-15 14:29:03 +0000

    Remove vertical test

--- tests.hs
+++ tests.hs
@@ -22,10 +22,6 @@ assert3InARowOnlyMiddleSurvives = assertConway
 	"3 in a row -> Middle survives"
 	[Cell 0 0, Cell 1 0, Cell 2 0] [Cell 1 0]
 
-assert3InAColumnOnlyMiddleSurvives = assertConway
-	"3 in a column -> Middle survives"
-	[Cell 0 0, Cell 0 1, Cell 0 2] [Cell 0 1]
-
 assert4InARowOnlyMiddleSurvives = assertConway
 	"3 in a column -> Middle survives"
 	[Cell 0 0, Cell 0 1, Cell 0 2, Cell 0 3] [Cell 0 1, Cell 0 2]
@@ -34,6 +30,5 @@ main = runTestTT $ TestList [
 		assertEmptyReturnsEmpty
 		,assertOneAliveDies
 		,assert3InARowOnlyMiddleSurvives
-		,assert3InAColumnOnlyMiddleSurvives
 		,assert4InARowOnlyMiddleSurvives
 	]
