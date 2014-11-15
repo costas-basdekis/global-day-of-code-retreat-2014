3754f400dd8ba4b2647100d6f2fbff02b066631e
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-15 10:15:39 +0000

    Incompled :(

--- Main.hs
+++ Main.hs
@@ -7,6 +7,13 @@ data Cell = Alive | Dead deriving (Show, Eq)
 type Conway = [[Cell]]
 
 nextConway :: Conway -> Conway
-nextConway [] = []
-nextConway [[_]] = [[Dead]]
-nextConway  x = x
+nextConway xss = map (\xs -> nextRow xs) xss
+--nextConway [] = []
+--nextConway [[_]] = [[Dead]]
+--nextConway  x = x
+
+nextRow :: [Cell] -> [Cell]
+NextRow (x:xs) = Dead : (doRest xs)
+
+--nextCell :: Cell -> [[Cell]] -> Cell
+--nextCell = undefined
