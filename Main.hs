1ddcda4c15bbaf358a138f4220eb0a1a437d3dce
Costas Basdekis <costas.basdekis@gmail.com>
2014-11-15 12:11:00 +0000

    More cheaky programmer

--- Main.hs
+++ Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
+import Data.List
+
 main = do
 	putStrLn "hello, world"
 
-data Cell = Cell {x :: Int, y :: Int} deriving (Eq, Show)
+data Cell = Cell {x, y :: Int} deriving (Eq, Show, Ord)
 
 type Conway = [Cell]
 
 nextConway :: Conway -> Conway
-nextConway (x:y:xs) = [y]
+nextConway xs@(_:_:_) = (tail . reverse . tail) xs
 nextConway _ = []
 
 --isAlive :: Cell -> Conway -> Bool
--- test.hs
+++ test.hs
@@ -1,11 +1,14 @@
 module Conway_Test where
 
 import Main hiding (main)
+
 import Test.HUnit
+
+import Data.List
 
 assertConway message input expected = TestCase $ assertEqual
 	message
-	expected (nextConway input)
+	(sort expected) (sort (nextConway input))
 
 assertEmptyReturnsEmpty = assertConway
 	"Empty -> Empty"
@@ -19,8 +22,13 @@ assert3InARowLeavesCentralAlive = assertConway
 	"3 In A Row -> Central Alive"
 	[Cell 0 0, Cell 1 0, Cell 2 0] [Cell 1 0]
 
+assert4InARowLeavesCentralAlive = assertConway
+	"4 In A Row -> Central Alive"
+	[Cell 0 0, Cell 1 0, Cell 2 0, Cell 3 0] [Cell 1 0, Cell 2 0]
+
 main = runTestTT $ TestList [
-		assertEmptyReturnsEmpty,
-		assertAliveGetsDead,
-		assert3InARowLeavesCentralAlive
+		assertEmptyReturnsEmpty
+		,assertAliveGetsDead
+		,assert3InARowLeavesCentralAlive
+		,assert4InARowLeavesCentralAlive
 	]
